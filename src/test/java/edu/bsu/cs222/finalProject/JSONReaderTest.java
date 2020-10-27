5ba34c5f4731cfba80e9a198507ce570f2c17b10
Cameron <caslash@bsu.edu>
2020-10-27 15:01:10 -0400

    Switched from live data to example JSON files for tests in the JSONReaderTest class.

--- src/test/java/edu/bsu/cs222/finalProject/JSONReaderTest.java
+++ src/test/java/edu/bsu/cs222/finalProject/JSONReaderTest.java
@@ -2,6 +2,8 @@ package edu.bsu.cs222.finalProject;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
+import java.io.InputStream;
+
 public class JSONReaderTest {
 
     @Test
@@ -13,24 +15,22 @@ public class JSONReaderTest {
 
     @Test
     public void testGetLatitude() throws Exception {
-        GeocodeConnector geocodeConnector = new GeocodeConnector("1401 West Neely Muncie IN");
-        GeocodeParser geocodeParser = new GeocodeParser(geocodeConnector.geocodeInputstream());
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("Location.json");
+        GeocodeParser geocodeParser = new GeocodeParser(inputStream);
         Assertions.assertEquals("40.2033498", geocodeParser.getLatLng().getLatitude());
     }
 
     @Test
     public void testGetLongitude() throws Exception {
-        GeocodeConnector geocodeConnector = new GeocodeConnector("1401 West Neely Muncie IN");
-        GeocodeParser geocodeParser = new GeocodeParser(geocodeConnector.geocodeInputstream());
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("Location.json");
+        GeocodeParser geocodeParser = new GeocodeParser(inputStream);
         Assertions.assertEquals("-85.40260479999999", geocodeParser.getLatLng().getLongitude());
     }
 
     @Test
     public void testGetNames() throws Exception {
-        GeocodeConnector geocodeConnector = new GeocodeConnector("1401 West Neely Muncie IN");
-        GeocodeParser geocodeParser = new GeocodeParser(geocodeConnector.geocodeInputstream());
-        PlaceConnector placeConnector = new PlaceConnector(geocodeParser.getLatLng().getLatitude(), geocodeParser.getLatLng().getLongitude(), "restaurant", "1");
-        PlaceParser placeParser = new PlaceParser(placeConnector.placeInputstream());
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("Places.json");
+        PlaceParser placeParser = new PlaceParser(inputStream);
         Assertions.assertEquals("Amazing Joe's Grill", placeParser.getPlaceNames().get(0).getName());
     }
 }
