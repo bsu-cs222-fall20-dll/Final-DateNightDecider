81d6e05e37c6ac7d963ed906e1a16a3fd738d803
Cameron <caslash@bsu.edu>
2020-11-01 22:29:50 -0500

    Removed unused import statements.

--- src/main/java/edu/bsu/cs222/finalProject/PlaceParser.java
+++ src/main/java/edu/bsu/cs222/finalProject/PlaceParser.java
@@ -9,7 +9,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.ArrayList;
-import java.util.Map;
 
 @SuppressWarnings("deprecation") //Gets rid of warnings for soon-to-be obsolete classes in the API
 public class PlaceParser {
